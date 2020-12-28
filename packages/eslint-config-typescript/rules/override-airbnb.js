8aca8294e98285d8edd45453c5d47b033deead02
djaler <djaler1@gmail.com>
2020-12-28 17:01:07 +0300

    feat: initial

---
+++ packages/eslint-config-typescript/rules/override-airbnb.js
@@ -0,0 +1,16 @@
+const baseAirbnbOverrides = require('@djaler/eslint-config-base/rules/override-airbnb');
+
+module.exports = {
+    ...baseAirbnbOverrides,
+
+    '@typescript-eslint/indent': baseAirbnbOverrides.indent,
+
+    '@typescript-eslint/lines-between-class-members': baseAirbnbOverrides['lines-between-class-members'],
+
+    '@typescript-eslint/no-use-before-define': [baseAirbnbOverrides['no-use-before-define'][0], {
+        ...baseAirbnbOverrides['no-use-before-define'][1],
+        enums: false,
+        typedefs: false,
+    }],
+};
+
