fc265c5a62acfce6d530253a2f2964a0c83b3f1e
djaler <djaler1@gmail.com>
2021-01-06 19:49:46 +0300

    fix: @typescript-eslint/no-unsafe-call rule disabled

--- packages/eslint-config-typescript/rules/override-typescript-eslint.js
+++ packages/eslint-config-typescript/rules/override-typescript-eslint.js
@@ -6,6 +6,7 @@ module.exports = {
     '@typescript-eslint/no-non-null-assertion': 'off',
 
     '@typescript-eslint/no-unsafe-assignment': 'off',
+    '@typescript-eslint/no-unsafe-call': 'off',
     '@typescript-eslint/no-unsafe-member-access': 'off',
 
     '@typescript-eslint/no-var-requires': 'off',
